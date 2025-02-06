8e3afe962f03db470f55f079b3e40ffacbf45e7d
Vinamra Garg <gargvinamra21@gmail.com>
2025-02-07 00:50:39 +0530

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,28 @@
-import { reactRouter } from "@react-router/dev/vite";
-import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
+import { reactRouter } from "@react-router/dev/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  server: {
+    fs: {
+      strict: false, // Helps avoid file limit errors
+    },
+  },
+  optimizeDeps: {
+    include: ["react", "react-dom"],
+    exclude: ["lucide-react"], // Prevents excessive file handling
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return "vendor";
+          }
+        },
+      },
+    },
+  },
 });
