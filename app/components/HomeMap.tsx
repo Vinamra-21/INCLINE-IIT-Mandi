d00102068efe68d2a1038de87f29aabce89311e6
Vinamra Garg <gargvinamra21@gmail.com>
2025-02-14 21:21:11 +0530

    fix

--- app/components/HomeMap.tsx
+++ app/components/HomeMap.tsx
@@ -1,30 +1,31 @@
 "use client";
-import { useEffect } from "react";
-import L from "leaflet";
-import { MapContainer, TileLayer } from "react-leaflet";
+// import { useEffect } from "react";
+// import L from "leaflet";
+// import { MapContainer, TileLayer } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 
 function MapContent() {
-  useEffect(() => {
-    // Fix Leaflet icon paths
-    delete L.Icon.Default.prototype._getIconUrl;
-    L.Icon.Default.mergeOptions({
-      iconRetinaUrl:
-        "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png",
-      iconUrl:
-        "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png",
-      shadowUrl:
-        "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png",
-    });
-  }, []);
+  // useEffect(() => {
+  //   // Fix Leaflet icon paths
+  //   delete L.Icon.Default.prototype._getIconUrl;
+  //   L.Icon.Default.mergeOptions({
+  //     iconRetinaUrl:
+  //       "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png",
+  //     iconUrl:
+  //       "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png",
+  //     shadowUrl:
+  //       "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png",
+  //   });
+  // }, []);
 
   return (
-    <MapContainer center={[20, 0]} zoom={2} className="h-full w-full z-0">
-      <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      />
-    </MapContainer>
+    <img src="map.png" className="h-full w-full z-0"></img>
+    // <MapContainer center={[20, 0]} zoom={2} className="h-full w-full z-0">
+    //   <TileLayer
+    //     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    //     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    //   />
+    // </MapContainer>
   );
 }
 
